3cf68bfacea613dca2a4ce6547ff5ec408d8132b
Francesc116 <info@rmsonce.com>
2023-11-28 19:36:11 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,1 @@
 let ws = new WebSocket("wss://websocket-p75n.onrender.com/:443");
-
-ws.addEventListener('open', (event) => {
-  console.log('Socket connection open!');
-  // alert('Successfully connected to socket server 🎉');
-  ws.send('pong');
-});
-
-ws.addEventListener('error', (error) => {
-    console.error('Error in the connection', error);
-    alert('error connecting socket server', error);
-});
-
-ws.addEventListener('close', (event) => {
-    console.log('Socket connection closed');
-    alert('closing socket server');
-});
