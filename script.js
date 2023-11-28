b9b0fc3f915c3971f2fc6b37130d805d7b2475ee
Francesc116 <info@rmsonce.com>
2023-11-28 19:39:10 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,27 @@
 let ws = new WebSocket("wss://websocket-p75n.onrender.com/:443");
+
+let controllTD = document.querySelector('.controllTD') ;
+controllTD.addEventListener('input', (event) => {
+  ws.send(JSON.stringify({ 'slider1': controllTD.value / 100 }));
+}, false);
+
+let controllTD2 = document.querySelector('.controllTD2') ;
+controllTD.addEventListener('input', (event) => {
+  ws.send(JSON.stringify({ 'slider2': controllTD2.value / 100 }));
+}, false);
+
+ws.addEventListener('open', (event) => {
+  console.log('Socket connection open!');
+  // alert('Successfully connected to socket server 🎉');
+  ws.send('pong');
+});
+
+ws.addEventListener('error', (error) => {
+    console.error('Error in the connection', error);
+    alert('error connecting socket server', error);
+});
+
+ws.addEventListener('close', (event) => {
+    console.log('Socket connection closed');
+    alert('closing socket server');
+});
