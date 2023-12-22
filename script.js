d2efa9f6264a4930d709a0dcba4b1997cdeda1a9
Timet-X <152591795+Timet-X@users.noreply.github.com>
2023-12-22 15:34:08 +0800

    Update script.js

--- script.js
+++ script.js
@@ -4,22 +4,20 @@ let controllTD3 = document.querySelector('.controllTD3');
 controllTD3.addEventListener('keypress', (event) => {
   if (event.key === 'Enter') {
     ws.send(JSON.stringify({ 'text': controllTD3.value }));
-    controllTD3.value = ''; // 清空文本框
+    controllTD3.value = ''; // 这里清空文本框
+    event.preventDefault(); // 阻止默认的回车行为（例如表单提交）
   }
 }, false);
 
 ws.addEventListener('open', (event) => {
   console.log('Socket connection open!');
-  // alert('Successfully connected to socket server 🎉');
   ws.send('pong');
 });
 
 ws.addEventListener('error', (error) => {
-    console.error('Error in the connection', error);
-    alert('error connecting socket server', error);
+  console.error('Error in the connection', error);
 });
 
 ws.addEventListener('close', (event) => {
-    console.log('Socket connection closed');
-    alert('closing socket server');
+  console.log('Socket connection closed');
 });
