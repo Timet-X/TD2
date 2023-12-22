36ded1c3d783e905ce02f1e4bce35df422c4e686
Timet-X <152591795+Timet-X@users.noreply.github.com>
2023-12-22 15:19:23 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,9 @@
 let ws = new WebSocket("wss://websocket-1.onrender.com/:443");
 
-let controllTD3 = document.querySelector('.controllTD3'); // 保留文本输入框的引用
-let submitButton = document.querySelector('#submitButton'); // 引用提交按钮
-
-// 提交按钮点击事件
-submitButton.addEventListener('click', (event) => {
-  event.preventDefault(); // 防止表单默认提交行为
-  ws.send(JSON.stringify({ 'text': controllTD3.value })); // 发送文本输入框的值
+let controllTD3 = document.querySelector('.controllTD3') ;
+controllTD3.addEventListener('input', (event) => {
+  ws.send(JSON.stringify({ 'text': controllTD3.value }));
 }, false);
-
-// WebSocket 的其他事件监听器...
 
 ws.addEventListener('open', (event) => {
   console.log('Socket connection open!');
